20908cc24afbe9c2bd9b23db4410c1637369823a
Gustavo <gusts92@gmail.com>
2020-07-04 18:11:27 -0300

    @mostly complete project

--- README.md
+++ README.md
@@ -1,22 +1,40 @@
-# nuxt-dev-to-clone
+# Dev.to clone built with NuxtJS
 
-> My mathematical Nuxt.js project
+> An articles aggregation app using [DEV.TO](https://dev.to) public [API](https://docs.dev.to/api/), demonstrating capabilities of [NuxtJS](https://nuxtjs.org) new [fetch()](https://nuxtjs.org/api/pages-fetch)
 
-## Build Setup
+See [live mode](https://dev-clone.nuxtjs.app/).
 
-``` bash
+[![Edit dev-to-clone-nuxt](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/github/bdrtsky/nuxt-dev-to-clone/tree/master/?fontsize=14&hidenavigation=1&theme=dark)
+
+## What You’ll Learn
+
+- use `$fetchState` for showing nice placeholders while data is fetching on the client side
+
+- use `keep-alive` and `activated` hook to efficiently cache API requests on pages that have already been visited
+
+- reuse the `fetch` hook with `this.$fetch()`
+
+- set `fetchOnServer` value to control when we need to render our data on the server side or not
+
+- find a way to handle errors from `fetch` hook.
+
+## Getting Started
+
+```sh
+# clone the project
+git clone https://github.com/bdrtsky/nuxt-dev-to-clone.git
+
 # install dependencies
-$ npm install
+npm install
 
-# serve with hot reload at localhost:3000
-$ npm run dev
+# start the project
+npm run dev
 
-# build for production and launch server
-$ npm run build
-$ npm start
-
-# generate static project
-$ npm run generate
+# go to http://localhost:3000
 ```
 
-For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
+Read full article: https://nuxtjs.org/blog/build-dev-to-clone-with-nuxt-new-fetch
+
+<p align="center">
+  <img src="https://raw.githubusercontent.com/bdrtsky/nuxt-dev-to-clone/master/static/demo.gif">
+</p>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,66 +1,47 @@
-const pkg = require('./package')
-
-module.exports = {
+export default {
   mode: 'universal',
-
-  /*
-  ** Headers of the page
-  */
   head: {
-    title: pkg.name,
+    title: 'Dev.to clone with NuxtJS',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Building a dev.to clone with Nuxt.js and new fetch() hook'
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Inter:400,500,600&display=swap'
+      }
     ]
   },
-
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-
-  /*
-  ** Global CSS
-  */
+  loading: false, // disable loading bar
   css: [
+    '~/assets/styles/reset.scss',
+    '~/assets/styles/base.scss',
+    '~/assets/styles/highlight.scss',
+    '~/assets/styles/app.scss'
   ],
-
-  /*
-  ** Plugins to load before mounting the App
-  */
+  styleResources: {
+    scss: ['~/assets/styles/tokens.scss']
+  },
   plugins: [
     '~/plugins/vue-placeholders.js',
     '~/plugins/vue-observe-visibility.client.js'
   ],
-
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
-
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-      
-    }
-  },
-
   buildModules: [
+    '@nuxtjs/eslint-module',
     '@nuxtjs/svg',
     '@nuxtjs/style-resources'
   ],
-
-  styleResources: {
-    scss: ['~/assets/styles/tokens.scss']
+  modules: ['nuxt-ackee'],
+  ackee: {
+    server: 'https://ackee.nuxtjs.com',
+    domainId: '6336379b-8d3e-4069-9d2e-897be6a7ed4e'
   }
 }
